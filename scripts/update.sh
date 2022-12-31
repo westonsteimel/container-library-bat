52382aed8783d005e83f40cd94108a851c801191
Weston Steimel <weston.steimel@gmail.com>
2022-12-31 15:42:34 +0000

    chore: update version update script
    Signed-off-by: Weston Steimel <weston.steimel@gmail.com>

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,8 +2,8 @@
 
 set -euo pipefail
 
-version=`curl --silent "https://api.github.com/repos/sharkdp/bat/releases/latest" | jq -e .tag_name | xargs`
-revision=`curl --silent "https://api.github.com/repos/sharkdp/bat/commits/${version}" | jq -e .sha | xargs`
+version=$(curl --silent "https://api.github.com/repos/sharkdp/bat/releases/latest" | jq -e .tag_name | xargs)
+revision=$(curl --silent "https://api.github.com/repos/sharkdp/bat/commits/${version}" | jq -e .sha | xargs)
 version=${version#"v"}
 echo "latest stable version: ${version}, revision: ${revision}"
 
@@ -16,7 +16,7 @@ git add stable/Dockerfile
 git diff-index --quiet HEAD || git commit --message "updated stable to version ${version}, revision: ${revision}"
 
 version="master"
-revision=`curl --silent "https://api.github.com/repos/sharkdp/bat/commits/${version}" | jq -e .sha | xargs`
+revision=$(curl --silent "https://api.github.com/repos/sharkdp/bat/commits/${version}" | jq -e .sha | xargs)
 echo "latest edge version: ${version}, revision: ${revision}"
 
 sed -ri \
